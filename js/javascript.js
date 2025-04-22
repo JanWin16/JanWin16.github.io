04dce68ca33d61c733d89c0b25542ef859002c52
JanWin16 <jawmail@web.de>
2025-04-22 15:19:41 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,7 @@ let map = L.map('map').setView([58.373523, 26.716045], 12);
 
 const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
-  attribution: '© OpenStreetMap contributors'
-});
+  attribution: 'OpenStreetMap contributors',
+})
 
-osm.addTo(map);
+osm.addTo(map)
