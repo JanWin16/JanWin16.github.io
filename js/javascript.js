43079ca63d810311c23bb76199383bef3ba8f2b4
JanWin16 <jawmail@web.de>
2025-04-24 14:13:59 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -7,16 +7,11 @@ const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 osm.addTo(map)
 
-// Add GeoJSON polygons layer for Tartu city districts
 async function addDistrictsGeoJson(url) {
-  const response = await fetch(url);
-  const data = await response.json();
-  const polygons = L.geoJson(data, {
-    style: {
-      color: '#3388ff',
-      weight: 2,
-      fillOpacity: 0.3
-    }
-  });
-  polygons.addTo(map);
+  const response = await fetch(url)
+  const data = await response.json()
+  const polygons = L.geoJson(data)
+  polygons.addTo(map)
 }
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+
